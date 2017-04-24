361f1c088fc8cfff80d0b7bed36b5fee53a391c3
Akshay Thejaswi <chickenbellyfinn@gmail.com>
2017-04-23 23:12:24 -0400

    remove disp

--- +lrelu/apply.m
+++ +lrelu/apply.m
@@ -2,7 +2,6 @@ function a = apply(n,param)
 %LRELU.APPLY transfer function to inputs
 
 % Copyright 2012-2015 The MathWorks, Inc.
-  disp(lrelu.alpha)
   a = max(0,n) + lrelu.alpha * min(0, n);
   a(isnan(n)) = nan;
 end
